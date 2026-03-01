b5fd0756f56895c3628239702f142aca19997805
stefflermate <steffler.mate2@gmail.com>
2026-03-01 12:16:12 +0100

    asdf
    asdf

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/app/staticfiles"
+STATIC_ROOT = "/tmp/staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
