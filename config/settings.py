d66fe88f7eba654453563896c69aab9cc6495479
stefflermate <steffler.mate2@gmail.com>
2026-03-01 11:54:12 +0100

    asdf
    asdfa

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database: use DATABASE_URL (postgres://...)
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=60,
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
