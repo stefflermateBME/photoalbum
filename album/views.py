530e61d939f263abfcff930a96eecd3aabac4216
stefflermate <steffler.mate2@gmail.com>
2026-03-01 17:13:47 +0100

    Update views.py

--- album/views.py
+++ album/views.py
@@ -55,7 +55,7 @@ def photo_delete(request, pk: int):
 
     return render(request, "album/photo_confirm_delete.html", {"photo": photo})
 
-# build trigger test
+# build trigger test 333
 def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
